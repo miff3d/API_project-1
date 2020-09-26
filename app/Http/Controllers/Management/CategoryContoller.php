b9f7b94cc47e971c0f621605cf742c64556b9634
miff3d <42211250+miff3d@users.noreply.github.com>
2020-09-26 19:28:20 +0300

    Update CategoryContoller.php
    small change made

--- app/Http/Controllers/Management/CategoryContoller.php
+++ app/Http/Controllers/Management/CategoryContoller.php
@@ -55,12 +55,7 @@ class CategoryContoller extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit($id)
     {
         $category = Category::find($id);
@@ -79,12 +74,7 @@ class CategoryContoller extends Controller
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
         //
